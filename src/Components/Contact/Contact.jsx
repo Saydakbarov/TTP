4b46523c6fb26bf693ad2a16bbce730f3ca3c227
Jasur-coder <jasursaydakbarov03@gmail.com>
2023-05-24 11:07:56 +0300

    contact fix

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -94,7 +94,7 @@ export default function Contact() {
               </Typography>
               <Typography
                 sx={{
-                  fontSize: "18px",
+                  fontSize: { xs: "14px", sm: "15px", md: "16px", lg: "18px" },
                   fontFamily: "Inter",
                   textTransform: "uppercase",
                 }}
